dfc96950a5506163f6c2481f45c52e8848734ba5
Ali hassaan <alihassaanamjad@gmail.com>
2025-05-13 13:11:10 -0400

    contact us section

---
+++ index.js
@@ -0,0 +1,25 @@
+
+var swiper = new Swiper(".projectSlider", {
+    slidesPerView: 3.2,
+    spaceBetween: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+/////
+$(window).scroll(function () {
+    var hT = $('#skill-bar-wrapper').offset().top,
+        hH = $('#skill-bar-wrapper').outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    if (wS > (hT + hH - 1.4 * wH)) {
+        jQuery(document).ready(function () {
+            jQuery('.skillbar-container').each(function () {
+                jQuery(this).find('.skills').animate({
+                    width: jQuery(this).attr('data-percent')
+                }, 2000);
+            });
+        });
+    }
+});
