5e90e69296bbfb139f2363ea7b6078a72a57a387
alihassaan12 <117427984+alihassaan12@users.noreply.github.com>
2026-07-01 20:07:27 +0000

    Fix sticky header background color

--- src/components/site/Layout.tsx
+++ src/components/site/Layout.tsx
@@ -27,7 +27,7 @@ export function SiteLayout({ children }: { children: ReactNode }) {
           }
         });
       },
-      { threshold: 0.08, rootMargin: "0px 0px -40px 0px" }
+      { threshold: 0, rootMargin: "0px 0px -10% 0px" }
     );
     targets.forEach((el) => io.observe(el));
     return () => io.disconnect();
